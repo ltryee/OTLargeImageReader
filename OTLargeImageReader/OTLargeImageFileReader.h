f6b3a776aa9c3a7b7a334677ce7ebb85771113be
openthread-mini <openfibers@gmail.com>
2015-06-04 16:19:33 +0800

    OTLargeImageFileReader comments updated

--- OTLargeImageReader/OTLargeImageFileReader.h
+++ OTLargeImageReader/OTLargeImageFileReader.h
@@ -26,12 +26,50 @@
  */
 + (CGSize)getImageEXIFSizeFromFilePath:(NSString *)filePath error:(NSError **)error;
 
+/**
+ *  Generate a thumbnail from a large image from disk, get rid of a terrible memory peak.
+ *
+ *  @param filePath     The large file to read.
+ *  @param maxPixelSize The max pixel size of the two edges of the image.
+ *  @param imageSize    If you already know the size of image, pass it to here. Otherwise pass CGSizeZero. \n If pass CGSizeZero, this method will read the EXIF info for image size.
+ *
+ *  @return The result thumbnail. Nil if generate thumbnail failed.
+ */
 + (UIImage *)thumbImageFromLargeFile:(NSString *)filePath withMaxPixelSize:(CGFloat)maxPixelSize imageSize:(CGSize)imageSize;
 
+/**
+ *  Generate a thumbnail from a large image from disk, get rid of a terrible memory peak.
+ *  Excute and callback in a background thread.
+ *
+ *  @param filePath     The large file to read.
+ *  @param maxPixelSize The max pixel size of the two edges of the image.
+ *  @param imageSize    If you already know the size of image, pass it to here. Otherwise pass CGSizeZero. \n If pass CGSizeZero, this method will read the EXIF info for image size.
+
+ *  @param callback     The callback of result image. Pass nil if generate thumbnail failed.
+ */
 + (void)thumbImageFromLargeFile:(NSString *)filePath withMaxPixelSize:(CGFloat)maxPixelSize imageSize:(CGSize)imageSize callback:(void(^)(UIImage *))callback;
 
+/**
+ *  Generate a thumbnail from a large image from disk, get rid of a terrible memory peak.
+ *
+ *  @param filePath     The large file to read.
+ *  @param minPixelSize The min pixel size of the two edges of the image.
+ *  @param imageSize    If you already know the size of image, pass it to here. Otherwise pass CGSizeZero. \n If pass CGSizeZero, this method will read the EXIF info for image size.
+ *
+ *  @return The result thumbnail. Nil if generate thumbnail failed.
+ */
 + (UIImage *)thumbImageFromLargeFile:(NSString *)filePath withMinPixelSize:(CGFloat)minPixelSize imageSize:(CGSize)imageSize;
 
+/**
+ *  Generate a thumbnail from a large image from disk, get rid of a terrible memory peak.
+ *  Excute and callback in a background thread.
+ *
+ *  @param filePath     The large file to read.
+ *  @param minPixelSize The min pixel size of the two edges of the image.
+ *  @param imageSize    If you already know the size of image, pass it to here. Otherwise pass CGSizeZero. \n If pass CGSizeZero, this method will read the EXIF info for image size.
+ 
+ *  @param callback     The callback of result image. Pass nil if generate thumbnail failed.
+ */
 + (void)thumbImageFromLargeFile:(NSString *)filePath withMinPixelSize:(CGFloat)minPixelSize imageSize:(CGSize)imageSize callback:(void(^)(UIImage *))callback;
 
 @end
